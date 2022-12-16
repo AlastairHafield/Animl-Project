96035a726dca920ca9f8f75b84082416945a5d3a
AlastairHatfield <alastairhatfield@live.com>
2022-12-16 07:27:11 +0000

    added script.js, created animal array

---
+++ home.js
@@ -0,0 +1,9 @@
+var articleHeadCont = document.getElementById('article-main');
+var articleH2 = document.getElementById('article-header');
+var articles = document.getElementById('articles');
+
+var getArticles = function(){
+
+}
+
+var animals = ['Tiger', 'Lion', 'Elephant', 'Leopard', 'Panther', 'Cheetah', 'Wolf', 'Jaguar', 'Hyena', 'Giraffe', 'Deer', 'Zebra', 'Gorilla', 'Monkey', 'Chimpanzee', 'Bear', 'Wild Boar', 'Hippopotamus', 'Kangaroo', 'Rhinoceros', 'Crocodile', 'Panda', 'Squirrel', 'Mongoose', 'Porcupine', 'Koala Bear', 'Wombat', 'Meerkat', 'Otter', 'Hedgehog', 'Possum', 'Chipmunk', 'Squirrel', 'Raccoon', 'Jackal', 'Hare', 'Mole', 'Rabbit', 'Alligator', 'Monitor Lizard', 'Oryx', 'Elk', 'Badger', 'Dinosaur', 'Pangolin', 'Mole', 'Okapi', 'Camel', 'Wild cat', 'Coyote', 'Bison', 'African Elephant', 'Aardvark', 'Antelope', 'Alpine Goat', 'Komodo Dragon', 'Bearded Dragon', 'Royal Bengal Tiger', 'Flying Squirrel', 'Emu', 'Eagle', 'Eel', 'Asiatic Lion', 'Armadillo', 'Beaver', 'Emperor Penguin', 'Baboon', 'Bat', 'Chameleon', 'Bull', 'Giant Panda', 'Chihuahua', 'Orangutan', 'Chinchillas', 'Hawk', 'Iguana', 'Ibis', 'Ibex', 'King Cobra', 'Jellyfish', 'Goose', 'Walrus', 'Seal', 'Skink', 'Markhor', 'Falcon', 'Bull Shark', 'Arctic Wolf', 'Owl', 'Bulbul', 'Bobcat', 'Guinea Pig', 'Yak', 'Reindeer', 'Moose', 'Puma', 'Okapi', 'Marten', 'Squirrel Monkey', 'Caracal'];
