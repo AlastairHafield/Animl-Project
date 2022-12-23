58ff7920e8c8eb2937c4ac40a78b66199f37f2c5
AlastairHatfield <alastairhatfield@live.com>
2022-12-23 15:04:37 +0000

    added embeded videos

--- mix/assets/mix.js
+++ mix/assets/mix.js
@@ -6,6 +6,24 @@ var button = document.getElementById("mix-button");
 var pageNum = 1;
 // Set the initial counter value
 var counter = 0;
+//video url
+var videoURL = "https://pixabay.com/api/videos/?key=" + API_KEY + "&category=animals"
+
+var getVideos = function(){
+  //Url for api with query parameters
+  fetch(videoURL)
+      .then(function (response){
+          if (response.ok) {
+              console.log(response);
+              response.json().then(function (data2) {
+                  var dataStore2 = JSON.stringify(data2);
+                  localStorage.setItem('dataStore2', dataStore2);
+                  displayData2();
+              });} else if (response.status === 400 || 402 || 404){
+                  return;
+}})};
+
+
 
 var getArticles = function(){
     //Url for api with query parameters
@@ -42,10 +60,34 @@ var displayData = function() {
   let dataText = localStorage.getItem('dataStore');
   let object = JSON.parse(dataText);
 
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 8; i++) {
     let imageUrl = object.hits[i].webformatURL;
     document.getElementById(`image${i + 1}`).src = imageUrl;
+    document.getElementById(`modal-image${i + 1}`).src = imageUrl;
   }
+
+  var hitsArray = object.hits;
+  var numLargeImageURLs = 0;
+  
+  for (var i = 0; i < hitsArray.length; i++) {
+    if (hitsArray[i].hasOwnProperty("largeImageURL")) {
+      numLargeImageURLs++;
+    }
+  }
+  
+  console.log(numLargeImageURLs);
+}
+
+var displayData2 = function() {
+  let dataText = localStorage.getItem('dataStore2');
+  let object = JSON.parse(dataText);
+
+  for (let i = 0; i < 8; i++) {
+    let videoUrl = object.hits[i].videos.tiny.url;
+    console.log(videoUrl)
+    document.getElementById(`video${i + 1}`).src = videoUrl;
+  }
+
   var hitsArray = object.hits;
   var numLargeImageURLs = 0;
   
@@ -59,18 +101,18 @@ var displayData = function() {
 }
 
 getArticles();
+getVideos();
 
-document.querySelector('#image1').addEventListener('click', function() {
-  document.getElementById('modal1').classList.toggle('opacity-0');
-  document.getElementById('modal1').classList.toggle('pointer-events-none');
-});
-document.querySelector('#image2').addEventListener('click', function() {
-  document.getElementById('modal2').classList.toggle('opacity-0');
-  document.getElementById('modal2').classList.toggle('pointer-events-none');
-});
+function toggleModal(modalId) {
+  document.getElementById(modalId).classList.toggle('opacity-0');
+  document.getElementById(modalId).classList.toggle('pointer-events-none');
+}
 
-  
-document.querySelector('.modal-close').addEventListener('click', function() {
-  document.querySelector('.modal').classList.toggle('opacity-0');
-  document.querySelector('.modal').classList.toggle('pointer-events-none');
-});
+for (let i = 1; i <= 8; i++) {
+  document.querySelector(`#image${i}`).addEventListener('click', function() {
+    toggleModal(`modal${i}`);
+  });
+  document.querySelector(`#modal-close${i}`).addEventListener('click', function() {
+    toggleModal(`modal${i}`);
+  });
+}
