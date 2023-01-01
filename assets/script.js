487da27c1ef043ff7cec58b79e90a5acf68ee538
joseph <josephcotterell@hotmail.co.uk>
2023-01-01 20:44:24 +0000

    added buttons for the animals and js code for the buttons

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,12 @@
-var animals = [
-  "Tiger",
-  "Lion",
-  "Elephant",
-  "Leopard",
-  "Panther",
-  "Cheetah",
-  "Wolf",
-  "Jaguar",
-  "Hyena",
-  "Giraffe",
-  "Deer",
-  "Zebra",
-  "Gorilla",
-  "Monkey",
-  "Chimpanzee",
-  "Bear",
-  "Wild Boar",
-  "Hippopotamus",
-  "Kangaroo",
-  "Rhinoceros",
-  "Crocodile",
-  "Panda",
-  "Squirrel",
-  "Mongoose",
-  "Porcupine",
-  "Koala Bear",
-  "Wombat",
-  "Meerkat",
-  "Otter",
-  "Hedgehog",
-  "Possum",
-  "Chipmunk",
-  "Dog",
-  "Cat",
-  "Spider",
-  "Snake",
-];
+var toggleBtn = document.querySelector("#toggleBtn");
+var divList = document.querySelector(".listHolder");
 
-var random = Math.floor(Math.random() * animals.length);
-
-apiKey =
-  "https://pixabay.com/api/?q=" +
-  random +
-  " &key=32156415-1868cf2c3bc46f4a76056c659";
+toggleBtn.addEventListener("click", () => {
+  if (divList.style.display === "none") {
+    divList.style.display; = 'block'
+    toggleBtn.innerHTML = 'hide list'
+  } else {
+    divList.innerHTML = "none";
+    toggleBtn.innerHTML = 'show list'
+  }
+});
