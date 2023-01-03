7aa8ccb32f1525ca11f147b373bc5e6f54dc6576
joseph <josephcotterell@hotmail.co.uk>
2023-01-03 21:45:56 +0000

    added background images for animals

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
-var toggleBtn = document.querySelector("#toggleBtn");
+var toggleBtn = document.querySelector("#dogTogglebtn");
 var divList = document.querySelector(".listHolder");
 
 toggleBtn.addEventListener("click", () => {
   if (divList.style.display === "none") {
-    divList.style.display; = 'block'
-    toggleBtn.innerHTML = 'hide list'
+    divList.style.display;
+    ("block");
+    toggleBtn.innerHTML = "hide list";
   } else {
     divList.innerHTML = "none";
-    toggleBtn.innerHTML = 'show list'
+    toggleBtn.innerHTML = "show list";
   }
 });
