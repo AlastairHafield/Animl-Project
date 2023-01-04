3c87335ee3bdc22f9fd03644f5064e51ec0fb398
AlastairHatfield <alastairhatfield@live.com>
2023-01-04 20:52:30 +0000

    javascript weather

--- home-page-2/home.js
+++ home-page-2/home.js
@@ -1,19 +1,34 @@
 //how do i link this to html
 
-navigator.geolocation.getCurrentPosition(function(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
+//async function getWeatherData() {
+  // await navigator.geolocation.getCurrentPosition(async function(position) {
+  //   var lat = position.coords.latitude;
+  //   var lon = position.coords.longitude;
     
-    var apiKey = 'e998301ef612bdf80b092d061576a6ab';
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
-    
-    fetch(weatherUrl)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        var weatherData = data.weather[0].description;
-        document.getElementById('city').innerHTML = weatherData;
-      });
-  });
+  //   var apiKey = 'e998301ef612bdf80b092d061576a6ab';
+  //   var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+  //   console.log(weatherUrl);
+  //   try {
+  //     const response = await fetch(weatherUrl);
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       localStorage.setItem('dataStorage', data);
+  //       //displayData(data);
+  //     } else if (response.status === 404) {
+  //       clearInput();
+  //       return;
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // });
+//}
+
+//getWeatherData();
+
+    //  then (data) 
+        //var weatherData = data.weather[0].description;
+      //  document.getElementById('city').innerHTML = weatherData;
+   
+
   
